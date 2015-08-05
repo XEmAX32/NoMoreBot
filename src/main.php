60d8b53ef3bdbd42dae7e1c8f7776aebd6533e4a
XEmAX32 <XEmAX32@gmail.com>
2015-08-05 22:53:59 +0200

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,1 +1,23 @@
 <php?
+namespace NoMoreBot
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\PlayerPreLoginEvent;
+use pocketmine\event\Listener;
+use pockermine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener{
+
+private $IPs = [];
+private $ipBanList;
+
+public funtion onEnable(){
+$this->getServer->getPluginManager()->registerEvents($this, $this);
+$this->getLogger->info(TextFormat::BLUE . " [NoMoreBot enabled]");
+}
+
+public function onDisable(){
+$this->getLogger()->info(TextFormat::BLUE . " [NoMoreBot disabled]");
+}
+
+public function 
