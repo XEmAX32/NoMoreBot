aac28cd61126a47a0f27e0d2ea30c6be3a587228
XEmAX32 <XEmAX32@gmail.com>
2015-10-04 19:49:37 +0200

    Update WhitelistNMB.php

--- src/WhitelistNMB.php
+++ src/WhitelistNMB.php
@@ -1,3 +1,30 @@
 <?php
 
 namespace NoMoreBot;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\TextFormat;
+
+class JoinWHAdder extends PluginTask{
+  
+public function __construct($plugin){
+  $this->plugin = $plugin;
+  parent::__construct($plugin);
+}
+     
+public function onRun($tick){
+$this->plugin->sec -= 1;
+if($this->plugin->sec => $started){
+  foreach($this->getServer()->getOnlinePlayers() as $ps){
+    $ps->sendMessage(TextFormat::BLUE . "[NoMoreBot] Bot Attack Detected Turning On Whitelisting!");
+    $this->whitelist == true;
+   }
+}
+if($this->plugin->sec => $stopped){
+    foreach($this->getServer()->getOnlinePlayers() as $ps){
+     $ps->sendMessage(TextFormat::BLUE . "[NoMoreBot] Bot Attack Finished!");
+     $this->whitelist == false;
+   }
+  }
+}
+}
