05f2d05a7910a1c2c06ba93c072ee34902f050e3
XEmAX32 <XEmAX32@gmail.com>
2015-10-04 18:03:12 +0200

    Update JoinWHAdder.php

--- src/JoinWHAdder.php
+++ src/JoinWHAdder.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace NoMoreBot;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\TextFormat;
+
+class JoinWHAdder extends PluginTask{
+  
+     public function __construct($plugin){
+          $this->plugin = $plugin;
+          parent::__construct($plugin);
+     }
+     public function onRun($tick){
+          $this->plugin->sec -= 1;
+            if($this->plugin->sec => $time){
+              $wl->set($name);
+              $player->sendMessage(TextFormat::BLUE . "[NoMoreBot] You Have Been Added On Whitelist");
+            }
+         }
+      }
